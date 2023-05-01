7a507afd3d062f5e528e810f60ed4076ecb2ed32
Dmitrii Borisov <rundoom1337@gmail.com>
2023-05-02 00:22:46 +0700

    Show item name

--- character/ItemPicker.gd
+++ character/ItemPicker.gd
@@ -7,8 +7,9 @@ var items_in_area: Array[BaseItem] = []
 
 func _on_area_entered(area: Area2D) -> void:
 	items_in_area.append(area)
-	
+	area.show_name()
 
 
 func _on_area_exited(area: Area2D) -> void:
 	items_in_area.erase(area)
+	area.hide_name()
--- items/BaseItem.gd
+++ items/BaseItem.gd
@@ -2,3 +2,15 @@ extends Area2D
 class_name BaseItem
 
 @export var item_name: StringName
+
+
+func _ready() -> void:
+	$Name.text = item_name
+
+
+func show_name():
+	$Name.show()
+	
+	
+func hide_name():
+	$Name.hide()
