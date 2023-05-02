8c47f59c5747f04686334d2df1444670b3a80a7f
Dmitrii Borisov <rundoom1337@gmail.com>
2023-05-02 22:43:19 +0700

    Destructable tiles

--- character/Character.gd
+++ character/Character.gd
@@ -12,7 +12,7 @@ func _physics_process(delta: float) -> void:
 	var hrz_dir := Input.get_axis("ui_left", "ui_right")
 	var vrt_dir := Input.get_axis("ui_up", "ui_down")
 	
-	velocity = Vector2(hrz_dir, vrt_dir) * SPEED
+	velocity = Vector2(hrz_dir, vrt_dir).normalized() * SPEED
 	move_and_slide()
 
 
---
+++ character/WallCrusher.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+
+@onready var space_state = get_world_2d().direct_space_state
+
+
+func _on_body_entered(body: TileMap) -> void:
+	var phys_params = PhysicsShapeQueryParameters2D.new()
+	phys_params.shape = $CollisionShape2D.shape
+	phys_params.transform = $CollisionShape2D.global_transform
+	phys_params.collision_mask = 4
+	
+	var collide_points = space_state.collide_shape(phys_params, 64)
+	
+	for it in collide_points: body.erase_cell(0, body.local_to_map(it))
+	
