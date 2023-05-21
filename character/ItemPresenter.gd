2163367b736bd1c4ab6016b72416117223814189
Dmitrii Borisov <rundoom1337@gmail.com>
2023-05-21 21:20:21 +0700

    Inventory clickable

--- character/ItemPresenter.gd
+++ character/ItemPresenter.gd
@@ -2,8 +2,13 @@ extends PanelContainer
 class_name ItemPresenter
 
 
-var item: BaseItem:
+var item: BaseItem = (load("res://items/base_item.tscn") as PackedScene).instantiate():
 	set(value):
 		item = value
 		$TextureRect.texture = item.image
 
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("LMB"):
+		print(item.item_name)
