6a75b76b2350e144ca0c841a8975c889cfa93728
Dmitrii Borisov <rundoom1337@gmail.com>
2023-05-28 22:21:52 +0700

    Tree drops wood

--- test_level/TileLevel.gd
+++ test_level/TileLevel.gd
@@ -8,22 +8,15 @@ var ItemSc = preload("res://items/base_item.tscn")
 
 func disrupt_tile(tile_pos: Vector2i) -> void:
 	if get_cell_atlas_coords(0, tile_pos) == Vector2i(-1, -1): return
-#	var tile_hp = disrupted_tiles.get(tile_pos)
-	
-#	if tile_hp == null:
-#		var cell_data := get_cell_tile_data(0, tile_pos)
-#		var hp = cell_data.get_custom_data("hp")
-#		disrupted_tiles[tile_pos] = cell_data.get_custom_data("hp")
+
 	var tile_data = get_cell_tile_data(0, tile_pos)
-#	disrupted_tiles[tile_pos] -= dmg
-#	if disrupted_tiles[tile_pos] <= 0:
+
 	var loot = ItemSc.instantiate()
 	loot.item_name = tile_data.get_custom_data("item_name")
 	loot.global_position = map_to_local(tile_pos)
 	loot.image = tile_data.get_custom_data("icon")
 	add_child(loot)
 	erase_cell(0, tile_pos)
-#	disrupted_tiles.erase(tile_pos)
 
 
 func get_tile_hp(tile_pos: Vector2i) -> int:
--- tree.gd
+++ tree.gd
@@ -2,6 +2,7 @@ extends StaticBody2D
 class_name InteractableObject
 
 
+var ItemSc = preload("res://items/base_item.tscn")
 var FoodSc = preload("res://items/food.tscn")
 @onready var world := get_tree().get_first_node_in_group("world") as TileLevel
 
@@ -10,3 +11,11 @@ func activate() -> void:
 	var food := FoodSc.instantiate() as BaseItem
 	food.global_position = global_position
 	world.add_child(food)
+
+
+func _on_tree_exiting() -> void:
+	var wood := ItemSc.instantiate() as BaseItem
+	wood.item_name = "wood"
+	wood.image = load("res://items/wood.tres")
+	wood.global_position = global_position
+	world.add_child.call_deferred(wood)
